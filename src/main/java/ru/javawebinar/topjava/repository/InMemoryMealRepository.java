00e526f2b2518efc0dd0f7a96dc18652eb0769e1
grantist <kubmet23@gmail.com>
2020-02-18 22:39:09 +0300

    InMemoryMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/InMemoryMealRepository.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
 public class InMemoryMealRepository implements MealRepository {
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
